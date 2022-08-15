2cd302a79117f75b9154ab16e08deaa996205f0d
Aleksandr Yermolaev <sanyagt50@gmail.com>
2022-08-15 21:39:38 +0300

    feat: resolve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,25 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+		this.range = Array.from({length: max - min + 1}, (a, b) => b + min);
+		this.low = this.range[0];
+		this.high = this.range[this.range.length - 1];
     }
 
     guess() {
-
+		this.midIndex = Math.floor(this.range.length / 2);
+		this.mid = this.range[this.midIndex];
+		return this.mid;
     }
 
     lower() {
-
+		this.high = this.range[this.midIndex];
+		this.range = this.range.slice(this.range.indexOf(this.low), this.range.indexOf(this.high) + 1);
     }
 
     greater() {
-
+		this.low = this.range[this.midIndex];
+		this.range = this.range.slice(this.range.indexOf(this.low), this.range.indexOf(this.high) + 1);
     }
 }
 
